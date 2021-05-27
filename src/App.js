d6b0d650c09b2b49c5a2c525f9fe10a8766d26b0
iulianstan <iulian1226@gmail.com>
2021-05-27 16:05:07 +0200

    src/App.js Update infos- Imports and start of App function

--- src/App.js
+++ src/App.js
@@ -1,3 +1,11 @@
+// Import React, useState and Fragment from "react"
+import React, { useState } from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import List from "./components/List";
+import ToDos from "./components/ToDos";
+
+// Function App
 function App() {
   return <div className="App"></div>;
 }
