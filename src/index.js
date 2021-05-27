df26512c23390a3113df5687f50ef8edd7cd964c
iulianstan <iulian1226@gmail.com>
2021-05-27 14:01:57 +0200

    src/index.js- Import main.scss file and added some comments

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
+// Import React and ReactDOM
 import React from "react";
 import ReactDOM from "react-dom";
+
+// Import App.js
 import App from "./App";
 
+// Import SASS
+import "./sass/main.scss";
+
+// ReactDOM render
 ReactDOM.render(
   <React.StrictMode>
     <App />
