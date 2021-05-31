0671c8d710530925ec79d2e82ed6aae718cd7182
iulianstan <iulian1226@gmail.com>
2021-05-31 11:29:48 +0200

    src/App.js- Created App function

--- src/App.js
+++ src/App.js
@@ -7,7 +7,33 @@ import ToDos from "./components/ToDos";
 
 // Function App
 function App() {
-  return <div className="App"></div>;
+  const [list, setList] = useState([]);
+  const remove = (itemToRemove) => {
+    // itemToRemove has the id for that specific item
+    const newArr = list.filter((item) => item.id !== itemToRemove);
+    setList(newArr);
+  };
+
+  const check = (itemToCheck) => {
+    // itemToCheck has the id for that specific item
+    setList((prevState) => {
+      return prevState.filter((item) => {
+        if (item.id === itemToCheck) item.done = !item.done;
+        return item;
+      });
+    });
+  };
+  // React Fragments allow to wrap multiple elements without adding an extra node to the DOM
+  // This can be useful when rendering multiple child elements/components in a single parent component
+  return (
+    <React.Fragment>
+      <Header />
+      <List setList={setList} />
+      <ToDos list={list} remove={remove} check={check} />
+      <Footer />
+    </React.Fragment>
+  );
 }
 
+// Export default App
 export default App;
