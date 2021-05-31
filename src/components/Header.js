c23fec4aa250e88b827e6d73e65d40ceb502aabc
iulianstan <iulian1226@gmail.com>
2021-05-31 11:38:16 +0200

    src/components/Header.js- Created Header function

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+// Header function
+const Header = () => {
+  // Print Header value
+  return (
+    <div>
+      <h1>Do you wanna ToDo something?</h1>
+    </div>
+  );
+};
+
+// Export default Header
+export default Header;
